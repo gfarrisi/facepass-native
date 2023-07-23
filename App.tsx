e3291c05fc28bbf65b6b3c251113bee42cc5b763
Gabriella Farrisi <gabriellafarrisi@MacBook-Pro-2.local>
2023-07-23 05:40:33 +0200

    change startup

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ export type Views =
   | 'successs';
 
 const App: React.FC = () => {
-  const [view, setView] = useState<Views>('waiting');
+  const [view, setView] = useState<Views>('frontCamera');
   const { address } = useEvmAddress();
 
   useEffect(() => {
